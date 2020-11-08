030f175adb194ecf64c8455ecc655a4a16b54b08
Peter Lisak <peter.lisak@gmail.com>
2020-11-08 23:17:24 +0100

    Refactor PlaylistLibraryProvider

--- mopidy_playlist/backend.py
+++ mopidy_playlist/backend.py
@@ -1,11 +1,11 @@
 import logging
-import urllib
+import os
+import urllib.parse
 
 import pykka
 from mopidy import backend, exceptions
 from mopidy.audio import scan, tags
 from mopidy.internal import http, playlists
-from mopidy.models import Track
 
 import mopidy_playlist
 
@@ -32,17 +32,10 @@ class PlaylistBackend(pykka.ThreadingActor, backend.Backend):
 
 
 class PlaylistLibraryProvider(backend.LibraryProvider):
-    def lookup(self, uri):
-        logger.info("Looking up playlist %s", uri)
-        if uri.startswith("pl:"):
-            url = uri[3:]
-        elif uri.startswith("playlist:"):
-            url = uri[9:]
-        else:
-            url = uri
-
+    def _parse_playlist(self, uri):
+        """Fetch and parse playlist from uri."""
         content = http.download(
-            self.backend.session, url, timeout=self.backend.timeout / 1000
+            self.backend.session, uri, timeout=self.backend.timeout / 1000
         )
 
         if content is None:
@@ -53,54 +46,68 @@ class PlaylistLibraryProvider(backend.LibraryProvider):
             return []
         uris = playlists.parse(content)
         if not uris:
+            logger.warning("Failed playlist from URI %s.", uri)
+            return []
+
+        return uris
+
+    def _scan_track(self, uri):
+        try:
+            scan_result = self.backend.scanner.scan(
+                uri, timeout=self.backend.timeout
+            )
+        except exceptions.ScannerError as exc:
             logger.warning(
-                "Failed playlist from URI %s.",
+                "GStreamer failed scanning URI (%s): %s %s",
+                uri,
+                exc,
+                self.backend.timeout,
+            )
+            return
+
+        has_interesting_mime = (
+            scan_result.mime is not None
+            and not scan_result.mime.startswith("text/")
+            and not scan_result.mime.startswith("application/")
+        )
+        if scan_result.playable or has_interesting_mime:
+            logger.debug(
+                "Unwrapped potential %s stream: %s",
+                scan_result.mime,
                 uri,
             )
-            return [Track(uri=uri)]
+            name = os.path.basename(urllib.parse.urlparse(uri).path)
+            track = tags.convert_tags_to_track(scan_result.tags).replace(
+                uri=uri, name=name, length=scan_result.duration
+            )
+            return track
+        else:
+            logger.warning(
+                "Unwrapped potential %s non-playable stream: %s",
+                scan_result.mime,
+                uri,
+            )
+
+    def lookup(self, uri):
+        logger.info("Looking up playlist %s", uri)
+        if uri.startswith("pl:"):
+            playlist_url = uri[3:]
+        elif uri.startswith("playlist:"):
+            playlist_url = uri[9:]
+        else:
+            playlist_url = uri
+
+        uris = self._parse_playlist(playlist_url)
 
         tracks = []
-        for u in uris:
+        # Limit max inspected/scanned items in playlist.
+        for u in uris[: self.backend.max_lookups]:
+            # TODO skip already inspected/scanned items
+            # TODO parse inner playlists recursively
             logger.debug("Found new URI %s in parsed playlist %s", u, uri)
-            u = urllib.parse.urljoin(url, u)
-            try:
-                scan_result = self.backend.scanner.scan(
-                    u, timeout=self.backend.timeout
-                )
-            except exceptions.ScannerError as exc:
-                logger.warning(
-                    "GStreamer failed scanning URI (%s): %s %s",
-                    uri,
-                    exc,
-                    self.backend.timeout,
-                )
-                scan_result = None
-
-            if scan_result:
-                has_interesting_mime = (
-                    scan_result.mime is not None
-                    and not scan_result.mime.startswith("text/")
-                    and not scan_result.mime.startswith("application/")
-                )
-                if scan_result.playable or has_interesting_mime:
-                    logger.debug(
-                        "Unwrapped potential %s stream: %s",
-                        scan_result.mime,
-                        uri,
-                    )
-                    track = tags.convert_tags_to_track(
-                        scan_result.tags
-                    ).replace(uri=u, length=scan_result.duration)
-                else:
-                    logger.debug(
-                        "Unwrapped potential %s inner playlist: %s",
-                        scan_result.mime,
-                        uri,
-                    )
-                    track = Track(uri=uri)
-            else:
-                logger.warning("Problem looking up %s", uri)
-                track = Track(uri=uri)
-            tracks.append(track)
+            u = urllib.parse.urljoin(playlist_url, u)
+            track = self._scan_track(u)
+            if track:
+                tracks.append(track)
 
         return tracks
